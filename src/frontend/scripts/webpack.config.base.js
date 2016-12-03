6f9be802af203a7e8fc309ea885d4a80a6d9c3c7
Allen.Zou <yalong.zou@hyku.com>
2016-12-03 10:53:11 +0800

    加入了NB的jade配置

--- src/frontend/scripts/webpack.config.base.js
+++ src/frontend/scripts/webpack.config.base.js
@@ -5,17 +5,22 @@ var path = require("path");
 var webpack = require("webpack");
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+// var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+// var PathRewriterPlugin = require('webpack-path-rewriter');
+var htmlExtractor = new ExtractTextPlugin("html", "[name].html");
 
 var config = {
     entry: {
-        mobile: "./src/mobile.js",
-        admin: "./src/admin.js",
-        pc: "./src/pc.js"
+        "mobile.js": "./src/mobile.js",
+        "admin.js": "./src/admin.js",
+        "pc.js": "./src/pc.js",
+        "pc": "./src/pc.jade"
     },
     output: {
         path: path.resolve(__dirname, "../entries/dist"),
         publicPath: "./dist/",
-        filename: "[name].js"
+        filename: "[name]"
     },
     resolve: {
         root: path.resolve(__dirname, "./src")
@@ -45,7 +50,21 @@ var config = {
             {
                 test: /\.html$/i,
                 loaders: ["html"]
-            }
+            },
+            {
+                test: /\.jade$/i,
+                loaders: ["pug-html"]
+            },
+            {
+                test: /pc\.jade$/i,
+                // loader: "source!jade-static"
+                // loader: ExtractTextPlugin.extract("pug-html")
+                loader: htmlExtractor.extract("html", "apply!pug?pretty=true")
+            },
+            // {
+            //     test: /\.jade$/i,
+            //     loaders: ["pug-loader"]
+            // }
         ]
     },
     postcss: function () {
@@ -54,12 +73,13 @@ var config = {
         };
     },
     plugins: [
+        htmlExtractor,
         new webpack.optimize.DedupePlugin(),
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
         }),
-        new webpack.optimize.OccurenceOrderPlugin()
+        new webpack.optimize.OccurenceOrderPlugin(),
     ]
 };
 
